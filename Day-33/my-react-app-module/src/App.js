94f5c64b74527c2ee181581e101eb645ed3db2ae
Morning <morning@MStarss-iMac.local>
2023-01-02 13:04:03 +0800

    jsday33

--- Day-33/my-react-app-module/src/App.js
+++ Day-33/my-react-app-module/src/App.js
@@ -29,6 +29,24 @@ function App() {
               <p className="text3"> Submitted by: <i class="fa-brands fa-twitter"></i> </p>
 
             </div>
+          </div>  <div className="container">
+            <img src="image-rose.png"></img>
+            <div className="text">
+              <p> <i class="fa-sharp fa-solid fa-eject"></i>  44 </p>
+              <p className="text1"> yellow Pail</p>
+              <p> On - demand sand castle construction expertise</p>
+              <p className="text3"> Submitted by: <i class="fa-brands fa-twitter"></i> </p>
+
+            </div>
+          </div>  <div className="container">
+            <img src="image-steel.png"></img>
+            <div className="text">
+              <p> <i class="fa-sharp fa-solid fa-eject"></i>  44 </p>
+              <p className="text1"> yellow Pail</p>
+              <p> On - demand sand castle construction expertise</p>
+              <p className="text3"> Submitted by: <i class="fa-brands fa-twitter"></i> </p>
+
+            </div>
           </div>
         </div>
 
