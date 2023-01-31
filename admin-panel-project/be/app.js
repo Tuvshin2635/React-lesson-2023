e9c162f6b7321f25cf7f661567d662c1e540be42
Morning <morning@MStarss-iMac.local>
2023-01-31 12:56:08 +0800

    day53

--- admin-panel-project/be/app.js
+++ admin-panel-project/be/app.js
@@ -1,1 +1,17 @@
 console.log("its my app.js");
+
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+/// configuration of moduls
+
+const app = express();
+const PORT = 8080;
+
+app.use(cors());
+app.use(express());
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost${PORT}`);
+});
--- admin-panel-project/fe/src/App.js
+++ admin-panel-project/fe/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Users from "./pages/Users";
+import SideBar from "./pages/sideBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> ADMIIN PANEL PROJECT MT </h1>
+      <SideBar />
+      <Routes>
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
