4741daa9846ca6d6b23dd5de50fbc09015e890e0
Munkhtuvshin Tuvshin <tuvshin@Munkhtuvshins-MacBook-Pro.local>
2023-04-06 07:25:15 +0800

    day92HW

--- Day-92-next-js-routing-ssr-csr-ssg/movie-review-fe/src/pages/navbar.tsx
+++ Day-92-next-js-routing-ssr-csr-ssg/movie-review-fe/src/pages/navbar.tsx
@@ -3,30 +3,36 @@ import styles from "@/styles/Home.module.css";
 
 function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <div className={styles.leftNavbar}>
-        <img
-          src="https://images.fandango.com/cms/assets/2d5a3340-be84-11ed-9d20-83ee649e98bd--rt25-logo-mainnav-161x50.svg"
-          alt="logo"
-        ></img>
-        <input
-          className={styles.input}
-          placeholder="search movies, TV, actors, more..."
-        />
+    <div>
+      <div className={styles.navbar}>
+        <div className={styles.leftNavbar}>
+          <img
+            src="https://images.fandango.com/cms/assets/2d5a3340-be84-11ed-9d20-83ee649e98bd--rt25-logo-mainnav-161x50.svg"
+            alt="logo"
+          ></img>
+          <input
+            className={styles.input}
+            placeholder="search movies, TV, actors, more..."
+          />
+        </div>
+        <div className={styles.rigthNav}>
+          <div className={styles.rightTop}>
+            <button>What's the Tomatometer®?</button>
+            <button>Critics</button>
+            <button>Login SignUp</button>
+          </div>
+          <div className={styles.rightBottom}>
+            <button>MOVIES</button>
+            <button>TV SHOWS</button>
+            <button>MOVIE TRIVIA</button>
+            <button>NEWS</button>
+            <button>SHOWTIMES</button>
+          </div>
+        </div>
       </div>
-      <div className={styles.rigthNav}>
-        <div className={styles.rightTop}>
-          <button>What's the Tomatometer®?</button>
-          <button>Critics</button>
-          <button>Login SignUp</button>
-        </div>
-        <div className={styles.rightBottom}>
-          <button>MOVIES</button>
-          <button>TV SHOWS</button>
-          <button>MOVIE TRIVIA</button>
-          <button>NEWS</button>
-          <button>SHOWTIMES</button>
-        </div>
+      <div className={styles.NavBottom}>
+        <p> TRENDING ON RT </p>
+        <button> The Super Mario Bros. Movie First Reviews</button>
       </div>
     </div>
   );
